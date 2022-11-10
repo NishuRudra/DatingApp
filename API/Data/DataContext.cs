802f93d9a3f9e5cd407dc6aab6908fe27bd2eef0
NitishKant <nitish.lpu@gmail.com>
2022-11-10 13:16:45 +0530

    changes

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -10,10 +10,7 @@ namespace API.Data
     public class DataContext:IdentityDbContext<AppUser,AppRole,int,
     IdentityUserClaim<int>,AppUserRole,IdentityUserLogin<int>,
     IdentityRoleClaim<int>,IdentityUserToken<int>>
-    
     {
-       
-
         public DataContext(DbContextOptions options):base(options)
         {
 
